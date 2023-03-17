29a0287058faa5c61e9370781d147a077e987203
moncef2006 <73278962+moncef2006@users.noreply.github.com>
2023-03-17 22:29:25 +0100

    change the contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -67,7 +67,7 @@ const Contact = () => {
 
   return (
     <div
-      className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}
+      className={`xl:mt-12 flex xl:flex-row flex-col-reverse justify-center items-center gap-10 overflow-hidden`}
     >
       <motion.div
         variants={slideIn("left", "tween", 0.2, 1)}
@@ -123,11 +123,6 @@ const Contact = () => {
           </button>
         </form>
       </motion.div>
-
-      <motion.div
-        variants={slideIn("right", "tween", 0.2, 1)}
-        className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]"
-      ></motion.div>
     </div>
   );
 };
